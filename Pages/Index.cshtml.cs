3a046a151dc4c0d788b956473faa42fefbaba955
Nicolas COMBE <nicolas.combe@acusurgical.com>
2026-03-26 14:19:45 +0100

    add MaintenanceMode with environment variables

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,15 +6,19 @@ namespace AzureQuizLab.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
+
+        public bool MaintenanceMode { get; set; }
 
         public void OnGet()
         {
-
+            MaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode", false);
         }
     }
 }
